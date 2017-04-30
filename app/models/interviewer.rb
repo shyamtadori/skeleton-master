314fb25e8acf6b7c93435f1212c23468d75263a0
Shyam <shyam.tadori@gmail.com>
2017-04-30 00:07:26 -0500

    upcoming two interviews of an interviewer with candidate data and questions data

--- app/models/interviewer.rb
+++ app/models/interviewer.rb
@@ -4,10 +4,10 @@ class Interviewer < ApplicationRecord
   has_many :interviews, through: :interview_participants
 
   def upcoming_interviews
-  	self.interviews.upcoming(Time.zone.now).includes(:candidate)
+  	self.interviews.upcoming(Time.zone.now).includes(:candidate, :questions)
   end
 
   def upcoming_two_interviews
-  	self.interviews.upcoming_two(Time.zone.now).includes(:candidate)
+  	self.interviews.upcoming_two(Time.zone.now).includes(:candidate, :questions)
   end
 end
--- test/models/interviewer_test.rb
+++ test/models/interviewer_test.rb
@@ -7,4 +7,11 @@ class InterviewerTest < ActiveSupport::TestCase
 
     assert_not_nil interviews.first.candidate.name
   end
+
+  test "upcoming interviews with questions data" do
+  	interviewer = interviewers(:hr)
+		interviews = interviewer.upcoming_two_interviews
+
+    assert_not_equal 0,interviews.first.questions.length
+  end
 end
